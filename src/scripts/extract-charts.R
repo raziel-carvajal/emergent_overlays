fe925d56662c33ae5205b93bb9aafdabd23cae11
Inti Gonzalez <inti.glez@gmail.com>
2016-06-27 19:21:57 +0200

    fixing R script

--- src/scripts/extract-charts.R
+++ src/scripts/extract-charts.R
@@ -32,7 +32,7 @@ powerlevels3 <- function(ds, ts = seq(step, max, by=step), max = 600, step=30) {
 }
 
 
-broadcastingTime <- function(ds) {
+broadcastingTime <- function(ds, simulation.time=600) {
 	v <- ds$vectordata
 	msg_sent <- ds$vectors[ ds$vectors$name == 'msg_sent:vector', ]$result
 	tmp <- subset(v, resultkey %in% msg_sent) # filter out vectors that are not msg_sent
@@ -45,12 +45,12 @@ broadcastingTime <- function(ds) {
 	list_of_received <- lapply(msg_received, function(p) subset(tmp2, resultkey == p)) # create a separate list for each broadcast_msg_received vector
 
 	sending.time <- sapply(id_msgs, function(id) min( unlist(lapply(list_of_sent, function(d)  subset(d, y == id, select=c(x))[[1]] )) ) )
-	reception.time <- sapply(id_msgs, function (id) max(sapply(list_of_received, function(d)  head(subset(d, y == id, select=c(x)), 1)[[1]] )) )
+	reception.time <- sapply(id_msgs, function (id) max(sapply(list_of_received, function(d)  head( rbind(subset(d, y == id, select=c(x)), 100*seq(simulation.time,simulation.time)), 1 )[[1]] )) )
 	
 	# compute number of message received at each location (coverage)
   rcv <- sapply(id_msgs, function(id) { sum( sapply(list_of_received, function(d) id %in% d$y ) ) } )
 
-	B.i.tmp <- sapply(id_msgs, function(id) sum(sapply(list_of_received, function(d)  sum(subset(d, y == 1, select=c(y))[[1]]) )))
+	B.i.tmp <- sapply(id_msgs, function(id) sum(sapply(list_of_received, function(d) length(subset(d, y == id, select=c(y))[[1]]) )))
 
 	broadcasting.time <- data.frame(
 			id = id_msgs, # session id
---
+++ src/scripts/run-one-testing.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# check if we have a path for omnet
+if [ ! -f "omnet.config" ]; then
+	while true; do
+		read -p "Do you want to download omnet++ 4.6 (y/n) " yn
+		case $yn in
+				[Yy]* ) ./download-omnet.sh; OMNET_PATH=../../tools/omnetpp-4.6 ; break;;
+				[Nn]* ) read -p "Please enter path to omnet++ 4.6 : " OMNET_PATH; break;;
+				* ) echo "Please enter y or n.";;
+		esac
+	done
+else
+	OMNET_PATH=`cat omnet.config`
+fi
+
+## OMNET_PATH=/home/inti/work/apps/omnetpp-4.6
+
+SHA1=`cat ../../revision.txt`
+
+CONFIG_PATH=../../experiments/configs
+
+# configuring path to omnet++
+source local-omnet-setenv.sh $OMNET_PATH
+
+# checking that everything is ready to execute the experiments
+./sanity-check.sh
+r=$?
+if [ $r -eq 0 ]; then
+	echo "${OMNET_PATH}" >> omnet.config
+fi
+
+# load the right version of the code
+echo "Checking out revision $SHA1"
+#./load-proper-version.sh ${SHA1}
+
+
+# compile applications' code
+./compile_protocols.sh "../protocols" "${OMNET_PATH}/samples/inet/" "../../built"
+error_code=$?
+echo "error code ===>>>>>> ${error_code}"
+if [ ${error_code} -ne 0 ]; then
+   echo "Error: problem compiling. Aborting"
+   exit 1
+fi
+
+if [ ! -d "../../results" ]; then
+    mkdir ../../results
+fi
+
+config="${CONFIG_PATH}/$1.ini"
+config_name="$1"
+echo "Executing : ${config}"
+./run-one-configuration.sh "${config}" "${config_name}" "${OMNET_PATH}/samples/inet" "../../built/gcc-debug/protocols"
+r=$?
+if [ $r -ne 0 ]; then
+  echo "Error: failure running simulation ${config_name}"
+  exit 1
+fi
+Rscript extract-charts.R ${CONFIG_PATH}/results/${config_name}-0 ../../results/${config_name}
