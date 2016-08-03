f953af18dcb9fc7bd358208f6651fbdce5226893
raziel-carvajal <raziel.carvajal@unine.ch>
2016-08-03 22:55:09 +0200

    Work in progress to have CDF

--- src/scripts/import-data.R
+++ src/scripts/import-data.R
@@ -19,8 +19,52 @@ import.data <- function(fileName) {
   df
 }
 
+get.attrSet <- function(dfNames, attri) {
+  set <- unique (
+    lapply(dfNames, function(name) {
+      header <- unlist(strsplit(name, '\"'))
+      item <- unlist(strsplit(header, '_'))
+      if (attri == "p") {
+        i <- tail(grep(attri, item), 1)
+        if (i %% 2 == 1) { i <- i + 1 }
+      } else { i <- head(grep(attri, item), 1) + 1 }
+      item[i]
+    })
+  )
+}
+
+plot.cdfs <- function(df, dfNames, sizes, algos, plotTitle) {
+  for (d in c('sparse', 'medium', 'dense')) {
+    for (s in sizes) {
+      pos <- grep(paste("n_", s, "_d_", d, sep=''), dfNames)
+      if (length(pos) != 0) {
+        keys <- unlist( lapply(pos, function(i) { dfNames[i] }) )
+        for (a in algos) {
+          i <- grep(a, keys)
+          if (length(i) != 0){
+            # TODO plot ECDF and add it to file per page
+            print(keys[i]) 
+          }
+        }
+      }
+    }
+  }
+}
+
+#x <- lapply(c('sparse', 'medium', 'dense'), function(d){ grep(d, dfNames) })
+#names(x) <- c('sparse', 'medium', 'dense')
+#df[[ dfNames[ x$dense ] ]]
+
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) == 1) {
-  x <- import.data(args[1])
-  x
+  df <- import.data(args[1])
+  dfNames <- names(df)
+  sizes <- get.attrSet(dfNames, "n")
+  algos <- get.attrSet(dfNames, "p")
+  plot.cdfs(df, dfNames, sizes, algos, "Broadcasting")
+  
+  #lapply(names(df), function(n){
+  #  x <- unlist(strsplit( unlist(strsplit(n, '\"'))[2], '_' ))
+  #  x
+  #})
 }
