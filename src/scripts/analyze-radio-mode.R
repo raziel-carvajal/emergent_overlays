9c8ba159c65e02c22b666468b725ed6e6f94a031
inti.glez <inti.glez@gmail.com>
2016-08-19 13:45:46 +0200

    working on report of energy consumption

--- src/scripts/analyze-radio-mode.R
+++ src/scripts/analyze-radio-mode.R
@@ -2,8 +2,12 @@ args <- commandArgs(trailingOnly=TRUE)
 
 print("Argument 1 is reception and argument 2 is transmission")
 
-reception <- read.csv(file=arg[1], header=TRUE, sep=",")
-transmission <- read.csv(file=arg[2], header=TRUE, sep=",")
+print(args[1])
+reception <- read.csv(file=args[1], header=TRUE, sep=",")
+
+print(args[2])
+transmission <- read.csv(file=args[2], header=TRUE, sep=",")
+
 consumption.receiving <- sapply( reception[,2], function(mode) if (mode==1) 0.002 else 0.01 )
 consumption.transmitting <- sapply( transmission[,2], function(mode) if (mode==1) 0.002 else 0.35 )
 
@@ -13,7 +17,11 @@ delta.t.recep <- reception[,1] - ( c(c(0), reception[,1])[-length(c( c(0), recep
 consumption.receiving <- c(c(0), consumption.receiving)[-length(c( c(0), consumption.receiving))]
 consumption.transmitting <- c(c(0), consumption.transmitting)[-length(c( c(0), consumption.transmitting))]
 
-sum(delta.t.recep * consumption.receiving)
-sum(delta.t.trans * consumption.transmitting)
+c.r <- sum(delta.t.recep * consumption.receiving)
+c.t <- sum(delta.t.trans * consumption.transmitting)
 
-sum(delta.t.recep * consumption.receiving) + sum(delta.t.trans * consumption.transmitting)
+total <- sum(delta.t.recep * consumption.receiving) + sum(delta.t.trans * consumption.transmitting)
+
+print(paste("Consumptions: receiving", c.r, "transmitting", c.t, "total", total))
+
+
