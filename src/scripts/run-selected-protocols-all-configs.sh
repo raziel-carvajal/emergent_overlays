64ff1779338985960857b27fc7cce8958be42c44
Inti Glez <Inti.Glez@gmail.com>
2016-09-21 11:44:30 +0200

    adding petty plotting

--- src/scripts/run-selected-protocols-all-configs.sh
+++ src/scripts/run-selected-protocols-all-configs.sh
@@ -45,7 +45,7 @@ for c in ${path_to_configs}*.ini ; do
 	density=`echo "$config_name" | awk -F "_" '{print $4 }'`
 	protocol=`echo "$config_name" | awk -F "_" '{print $12 }'`
 	if [ "$protocol" == "abba2" ] || [ "$protocol" == "mprt2" ] || [ "$protocol" == "flooding"  ] ; then
-    		if [ "${density}" -lt "18" ]; then
+    		if [ "${density}" -lt "78" ]; then
     			echo "This is one ${config_name}  ${nodes} ${density} ${protocol} "
 			    sem -j+0 --id "infocom2017" --no-notice ./run-one-configuration.sh ${c} ${OMNET_PATH}/samples/inet
     		fi
