fefe8467f1f4fc2cfcbf3a6ef6bf4aec4c7b142e
raziel-carvajal <raziel.carvajal@unine.ch>
2016-11-23 09:53:53 +0100

    Dependencies fro plotting with R were added

--- src/scripts/installing-dependencies.R
+++ src/scripts/installing-dependencies.R
@@ -4,5 +4,8 @@ if(length(new.packages)) {
   install.packages("../../tools/omnetpp_0.7-1.tar.gz", repos = NULL, type="source")
   install.packages("ggplot2", repos='http://cran.us.r-project.org')
   install.packages("argparse", repos='http://cran.us.r-project.org')
+  install.packages("reshape2", repos='http://cran.us.r-project.org')
+  install.packages("grid", repos='http://cran.us.r-project.org')
+  install.packages("gridExtra", repos='http://cran.us.r-project.org')
 }
 print ("omnetpp for R is installed")
