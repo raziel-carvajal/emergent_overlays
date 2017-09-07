9b9720630a8883f2423b3f33953581fc5a7ce138
raziel-carvajal <raziel.carvajal@gmail.com>
2017-09-07 23:16:45 +0200

    Merges 2 network files to create a topology with one PoI with a dense density of nodes and sparse density elswhere

--- src/scripts/merge-networks-files.sh
+++ src/scripts/merge-networks-files.sh
@@ -1,9 +1,9 @@
 #!/bin/bash - 
 #===============================================================================
 #
-#          FILE: create-asc-node-id.sh
+#          FILE: merge-networks-files.sh
 # 
-#         USAGE: ./create-asc-node-id.sh 
+#         USAGE: ./merge-networks-files.sh 
 # 
 #   DESCRIPTION: 
 # 
@@ -13,27 +13,28 @@
 #         NOTES: ---
 #        AUTHOR: YOUR NAME (), 
 #  ORGANIZATION: 
-#       CREATED: 09/06/2017 16:38
+#       CREATED: 09/07/2017 23:14
 #      REVISION:  ---
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
-
+EOS=`echo "END of ${0}"`
 getLine ()
 {
 	content=`head -${1} ${2} | tail -1`
 	return 0
 }	# ----------  end of function getLine  ----------
 
-
-if [ ${#} -lt 1 ] ; then
-	echo "USAGE: ${0} NED_FILE"
-	exit 1
+if [ ${#} -lt 3 ] ; then
+	echo -e "USAGE: ${0} SPARSE_NET DENSE_NET INC_X.\n${EOS}"; exit 1
 fi
 if [ ! -f ${1} ] ; then
-	echo "Topology file doesn't exist"
-	exit 1
+	echo -e "File with sparse network doesn't exist.\n${EOS}"; exit 1
 fi
+if [ ! -f ${2} ] ; then
+	echo -e "File with dense network doesn't exist.\n${EOS}"; exit 1
+fi
+
 firstTop=${1}
 rm -fr tmp; grep -n hostR ${firstTop} >tmp
 nodesNo=`cat tmp | wc -l`
@@ -53,10 +54,34 @@ if [ ${nodesNo} -gt 0 ] ; then
 		strToAdd="hostR${i}: CenterHost { ${content} }"
 		echo ${strToAdd} >>output
 	done
+	seconTop=${2}
+	incInX=${3}
+	grep hostR ${seconTop} >tmp
+	nodesf2=`cat tmp | wc -l`
+	if [ ${nodesf2} -gt 0 ] ; then
+		for (( j=0; j<${nodesf2}; j+=1 )); do
+			getLine $((${j} + 1)) tmp
+			headL=`echo ${content} | awk -F "p=" '{print $1}'`
+			restL=`echo ${content} | awk -F "p=" '{print $2}'`
+			poInX=`echo ${restL} | awk -F "," '{print $1}' \
+							| grep -Eo '[0-9]{1,5}.[0-9]{1,5}'`
+			poInY=`echo ${restL} | awk -F "," '{print $2}' \
+							| grep -Eo '[0-9]{1,5}.[0-9]{1,5}'`
+			poInX=$(bc <<< "${poInX}+${incInX}")
+			poInY=$(bc <<< "${poInY}+${incInX}")
+			let nextId=i+j
+			strToAdd="hostR${nextId}:`echo ${headL} | awk -F ":" '{print $2}'`"
+			strToAdd="${strToAdd}p=${poInX},${poInY}"'"'"); }"
+			echo ${strToAdd} >>output
+		done
+	else
+		echo "Second NET file (${2}) doesn't contain any host."
+		echo -e "Just sparse area will be present in final NED file."
+	fi
 	#add EOF
 	echo "}" >>output
 else
 	echo -e "NED_FILE (${1}) doesn't look like a NED file.\nAborting..."
 	exit 1
 fi
-echo "END of ${0}"
+echo "${EOS}"
