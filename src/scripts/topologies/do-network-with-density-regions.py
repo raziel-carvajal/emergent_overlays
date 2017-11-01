de464fbf07d025081c5a5a8276c828c12ff4e601
Raziel Carvajal-Gomez <raziel.carvajal@gmail.com>
2017-11-01 22:23:45 +0100

    New script to create connected topologies using 2 mobility models: random walk and levy-walk

--- src/scripts/topologies/do-network-with-density-regions.py
+++ src/scripts/topologies/do-network-with-density-regions.py
@@ -2,17 +2,23 @@
 import sys
 import math
 import argparse
-import itertools as iterT
 import networkx as nx
+from time import sleep
+import itertools as iterT
 import matplotlib.pyplot as plt
 from pymobility.models.mobility import random_waypoint
+from pymobility.models.mobility import heterogeneous_truncated_levy_walk
 
-FIRST_PLOT_NAME = "FirstPosition.pdf"
-STEPS_NO = 100
+FIRST_PLOT_NAME = "Position_"
+CONNECTED_OVERLAYS = 100
 MIN_VELOCITY = 0.1
 MAX_VELOCITY = 1.0
-WAITING_TIME = 1.0
+WAITING_TIME = 0.1
 MOBILITY_FILE= "mobility-trace"
+CONEC_DATASET= "connectivity-ds"
+CONEC_AT_INI_DS= "connectivity-ini-ds"
+CONNECTED = "connected"
+NOT_CONNECTED = "partitioned"
 
 def getArgs() :
 	p = argparse.ArgumentParser(description='Creates a network with N regions '+
@@ -69,14 +75,16 @@ def plotNodesPositions(pos, pltWidth) :
 	layout.plot(pltWidth, pltWidth, linestyle='', marker='.')
 	plt.scatter( [ pos[p]['x'] for p in pos], [ pos[p]['y'] for p in pos] )
 	plt.savefig(FIRST_PLOT_NAME)
-
-def plotOverlay(graph, positions) :
+	plt.clf()
+	
+def plotOverlay(iD, graph, positions) :
 	pTmp = {}
-	for k in range(1, len(positions) + 1):
-		pTmp[k] = [positions[k]['x'], positions[k]['y']]
+	for k, v in positions.iteritems():
+		pTmp[k] = [ v['x'], v['y'] ]
 	plt.subplot(111)
 	nx.draw_networkx(graph, pos=pTmp, node_size=10, with_labels=False)
-	plt.savefig(FIRST_PLOT_NAME)
+	plt.savefig(FIRST_PLOT_NAME + str(iD) + ".pdf")
+	plt.clf()
 
 def getOverlay(positions, transRange) :
 	g = nx.Graph()
@@ -98,25 +106,57 @@ def isConnected(g) :
 
 def savePositions(positions) :
 	with open(MOBILITY_FILE, 'a') as f:
-		for k in range(1, len(positions) + 1) :
-			l = str(k) + " " + str(positions[k]['x']) + " " + str(positions[k]['y'])
-			f.write(l + "\n")
+		for k, v in positions.iteritems() :
+			f.write(str(v['x']) + " " + str(v['y']) + "\n")
 
-#	plotInitialPos(latestP, args.cma_w)
+def addConnectEntry(fileName, entry) :
+	with open(fileName, 'a') as f :
+		f.write(entry + "\n")
+
 if __name__ == '__main__':
 	args = getArgs()
-	rw = random_waypoint(args.nodes_no, dimensions=(args.cma_w, args.cma_w),
-		velocity=(MIN_VELOCITY, MAX_VELOCITY), wt_max=WAITING_TIME)
 	tryNo = 0
 	hasPartitions = True
 	while hasPartitions :
 		latestP = getInitialPosition(args.nodes_no, args.regions, args.cma_w)
 		latestOv = getOverlay(latestP, args.Tx)
 		hasPartitions = not isConnected(latestOv)
+		if hasPartitions :
+			addConnectEntry(CONEC_AT_INI_DS, NOT_CONNECTED)
+		else :		
+			addConnectEntry(CONEC_AT_INI_DS, CONNECTED)			
+			plotOverlay(tryNo, latestOv, latestP)
 		tryNo = tryNo + 1
 		print("Try number to create P0 [" + str(tryNo) + "]")
-	tryNo = 0
+	args.nodes_no = len(latestP)
+	with open(MOBILITY_FILE, 'a') as f:
+		f.write(str(args.nodes_no) + "\n" + str(WAITING_TIME) + "\n")
+	savePositions(latestP)
 	print("First connected graph was created")
-	while tryNo < STEPS_NO :
-		savePositions(latestP)
+	tryNo = 0
+	connectedOvs = 0
+#	mobMod = random_waypoint(args.nodes_no, dimensions=(args.cma_w, args.cma_w),
+#		velocity=(MIN_VELOCITY, MAX_VELOCITY), wt_max=WAITING_TIME)
+	mobMod = heterogeneous_truncated_levy_walk(args.nodes_no, 
+		dimensions=(args.cma_w, args.cma_w), WT_EXP=-1.8, WT_MAX=WAITING_TIME)
+	while connectedOvs <= CONNECTED_OVERLAYS :
 		hasPartitions = True
+		while hasPartitions :
+			p_i = [ (k[0], k[1]) for k in next(mobMod)]
+			p_j = [ (k[0], k[1]) for k in next(mobMod)]
+			pDif= [ (p_j[k][0] - p_i[k][0], p_j[k][1] - p_i[k][1]) \
+				for k in range(0, args.nodes_no) ]
+			for k, v in latestP.iteritems() :
+				latestP[k] = { 'x': v['x'] + pDif[k-1][0], 'y': v['y'] + pDif[k-1][1] }
+			latestOv = getOverlay(latestP, args.Tx)
+			hasPartitions = not isConnected(latestOv)
+			if hasPartitions :
+				addConnectEntry(CONEC_AT_INI_DS, NOT_CONNECTED)
+			else :
+				addConnectEntry(CONEC_AT_INI_DS, CONNECTED)
+				savePositions(latestP)
+			tryNo = tryNo + 1
+			plotOverlay(tryNo, latestOv, latestP)
+			print("Try number to create Pi [" + str(tryNo) + "]")
+		connectedOvs = connectedOvs + 1
+		print("Number of connected overlays [" + str(connectedOvs) + "]")
