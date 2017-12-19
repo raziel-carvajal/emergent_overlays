7deec5c6c2807233a28697b4f944b8524d018ab5
Raziel Carvajal-Gomez <raziel.carvajal@gmail.com>
2017-12-19 19:46:25 +0100

    minor change to plot distribution of nodes positions

--- src/scripts/plot-mobility-dataset.R
+++ src/scripts/plot-mobility-dataset.R
@@ -2,10 +2,20 @@ library(reshape2)
 library(ggplot2)
 
 ds <- read.table("topologies/distribution-per-density", sep=" ")
-names(ds) <- c("xPos", "yPos", "zone")
-
-px <- ggplot(data=ds, aes(x=xPos, group=zone, colour=zone)) + stat_ecdf(geom = "step") + labs(x="Abscissa", y="Proportion of positions", title="CDF of abscissas of nodes positions over time (where peers moves every 100 ms)")
+names(ds) <- c("nodeId", "xPos", "yPos", "zone")
+ds <- subset(subset(ds, zone == 1), nodeId <= 100)
+pdf("x-dist.pdf")
+px <- ggplot(data=ds, aes(x=xPos, group=nodeId, colour=nodeId)) + 
+	stat_ecdf(geom = "step") + 
+	labs(x="Abscissa", y="CDF of abscissas", 
+	title="Node positions over time (where peers moves every 100 ms)")
 plot(px)
 
-py <- ggplot(data=ds, aes(x=yPos, group=zone, colour=zone)) + stat_ecdf(geom = "step") + labs(x="Ordinates", y="Proportion of positions", title="CDF of ordinates of nodes positions over time (where peers moves every 100 ms)")
-plot(py)
+pdf("y-dist.pdf")
+px <- ggplot(data=ds, aes(x=yPos, group=nodeId, colour=nodeId)) + 
+	stat_ecdf(geom = "step") + 
+	labs(x="Ordinates", y="CDF of ordinates", 
+	title="Node positions over time (where peers moves every 100 ms)")
+plot(px)
+
+
