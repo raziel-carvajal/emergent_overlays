6a4df8469159b229cc01e42f59900197701de8c3
raziel-carvajal <raziel.carvajal@gmail.com>
2018-07-31 13:13:36 +0200

    remove unused arguments

--- src/scripts/run-selected-protocols-all-configs.sh
+++ src/scripts/run-selected-protocols-all-configs.sh
@@ -1,111 +1,18 @@
 #!/bin/bash
 
-# sort of include files
-. utils.sh
-
-# the configuration to use
-CONFIG_PATH=../../experiments/configs/builtConfigs/
-MINIMUM_DENSITY=5
-MAXIMUM_DENSITY=15
-ALGORITHMS=()
-
-usage() {
-  echo "Usage $0: [-d DENSITY1] [-D DENSITY2] [-p PATH] [-a ALGORITHM]*"
-  echo ""
-  echo -e "\tDENSITY1: minimum density to consider (5 by default)\n"
-  echo -e "\tDENSITY2: maximum density to consider (15 by default)\n"
-  echo -e "\tPATH: used to located the configurations (../../experiments/configs/builtConfigs/ by default)\n"
-  echo -e "\tALGORITHM: an algorithm to consider\n"
-}
-
-
-# parsing parameters
-while getopts "d:D:p:a:h" opt; do
-  case $opt in
-    h)
-      usage
-      exit 0
-      ;;
-    d)
-      MINIMUM_DENSITY=$OPTARG
-      ;;
-    D)
-      MAXIMUM_DENSITY=$OPTARG
-      ;;
-    p)
-      CONFIG_PATH=$OPTARG
-      ;;
-    a)
-      ALGORITHMS+=($OPTARG)
-      ;;
-    \?)
-      exit 1
-      ;;
-  esac
-done
-
-##[ "${#ALGORITHMS[@]}" -eq "0" ] && echo "Ok, since there is no algorithm selected, we are done, bye!!!" && exit 0
-##
-### configuring path to omnet++
-##. download-omnet.sh
-##. local-omnet-setenv.sh ${OMNET_PATH}
-##
-### prepare result directory
-##if [ ! -d "../../results" ]; then
-##    mkdir ../../results
-##fi
-##rm -f ../../results/broadcastSession* \
-##      ../../results/duplicatedMsgs* \
-##      ../../results/batteryConsumption* \
-##      ../../results/networkCoverage* \
-##      ../../results/relays* \
-##      ../../results/coverage* \
-##      ../../results/macFramesReceived* \
-##      ../../results/macFramesSent* \
-##      ../../results/densityRelativeError* \
-##      ../../results/collisionsRelativeError* \
-##      ../../results/*.pdf \
-##      ../../results/summary.csv
-##
-##echo "Simulating"
-##
-##for c in ${CONFIG_PATH}*.ini ; do
-##	filename=$(basename "$c")
-##  my_substring="_forCol"
-##  if [[ ! "$filename" =~ "$my_substring" ]]; then
-##  	config_name="${filename%.*}"
-##    nodes=$(get_nrnodes_from_config_name $config_name)
-##    density=$(get_density_from_config_name $config_name)
-##    protocol=$(get_protocol_from_config_name $config_name)
-##    if [ "${density}" -ge "${MINIMUM_DENSITY}" ] && [ "${density}" -le "${MAXIMUM_DENSITY}" ]; then
-##      array_contains "$protocol" "${ALGORITHMS[@]}"
-##      if [ $? -eq 0 ]; then
-##        echo "This is one ${config_name}  ${nodes} ${density} ${protocol}"
-##        sem -j-1 --no-notice ./run-one-configuration.sh ${c} ${OMNET_PATH}/samples/inet 0
-##      fi
-##    fi
-##  fi
-##
-##done
-##
-##sem --wait --no-notice
-##
 echo "Creating aggregated results"
-
-cat ../../results/broadcastSession-n_* >> ../../results/broadcastSession
-cat ../../results/batteryConsumptionDistribution-n_* >> ../../results/batteryConsumptionDistribution
-cat ../../results/coverage-n_* >> ../../results/coverage
-cat ../../results/sentBroadcastMsgsDistribution-n_* >> ../../results/sentBroadcastMsgsDistribution
-cat ../../results/recvBroadcastMsgsDistribution-n_* >> ../../results/recvBroadcastMsgsDistribution
-cat ../../results/sentCtrlMsgsDistribution-n_* >> ../../results/sentCtrlMsgsDistribution
-cat ../../results/recvCtrlMsgsDistribution-n_* >> ../../results/recvCtrlMsgsDistribution
-cat ../../results/collisionsRelativeError-n_* >> ../../results/collisionsRelativeError
-cat ../../results/densityRelativeError-n_* >> ../../results/densityRelativeError
-cat ../../results/noderoles-n_* >> ../../results/noderoles
+cat ../../results/broadcastSession-n_* > ../../results/broadcastSession
+cat ../../results/batteryConsumptionDistribution-n_* > ../../results/batteryConsumptionDistribution
+cat ../../results/coverage-n_* > ../../results/coverage
+cat ../../results/sentBroadcastMsgsDistribution-n_* > ../../results/sentBroadcastMsgsDistribution
+cat ../../results/recvBroadcastMsgsDistribution-n_* > ../../results/recvBroadcastMsgsDistribution
+cat ../../results/sentCtrlMsgsDistribution-n_* > ../../results/sentCtrlMsgsDistribution
+cat ../../results/recvCtrlMsgsDistribution-n_* > ../../results/recvCtrlMsgsDistribution
+cat ../../results/collisionsRelativeError-n_* > ../../results/collisionsRelativeError
+cat ../../results/densityRelativeError-n_* > ../../results/densityRelativeError
+cat ../../results/noderoles-n_* > ../../results/noderoles
 
 echo "Plotting aggregated results"
-#	-run_algo algorithmTypeDistribution \
-# -final \
 Rscript pretty-plotting.R \
   -ds groundTruthDensityDist- \
 	-bs broadcastSession \
