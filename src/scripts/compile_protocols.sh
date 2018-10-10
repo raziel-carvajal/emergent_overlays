269e39700c051f5824bd773ed7a6b4cdc7485cb8
Raziel Carvajal-Gomez <raziel.carvajal@gmail.com>
2018-10-10 23:02:09 +0200

    make compilation procedure compatible with the new architecture

--- src/scripts/compile_protocols.sh
+++ src/scripts/compile_protocols.sh
@@ -1,54 +1,34 @@
 #!/bin/bash
 
-# configuring path to omnet++
-#. download-omnet.sh
-. local-omnet-setenv.sh ${OMNET_PATH}
-
-if [ $# -lt 2 ]; then
-    echo "Error: wrong number of parameters"
-    echo "Usage: $0 PATH_TO_PROTOCOLS OUT_PATH"
-    exit 1
-fi
-
-if [ ! -d $1 ]; then
-    echo "Error: $1 is not a valid directory nor a link. It must be the path to the directory that contains the protocols"
-    exit 1
-fi
-
-# load the right version of the code
-SHA1=`cat ../../revision.txt`
-echo "Checking out revision $SHA1"
-#./load-proper-version.sh ${SHA1}
-
-OUT_PATH=$2
-if [ ! -d ${OUT_PATH} ]; then
-    mkdir ${OUT_PATH}
-fi
-
-# tool to create makefiles
-OMNET_MAKEMAKE=opp_makemake
-# make application
-MAKE=make
-
-# variables
-INCLUDE="${OMNET_PATH}/samples/inet/src"
-PROTOCOLS=$1
-CORES=4
-
-# create base library that only includes project broadcasting
-cd "../base" && ${OMNET_MAKEMAKE} -f --deep -a -I${INCLUDE} -O ${OUT_PATH} -o protocol_base && make -j ${CORES}
-if [ $? -ne "0" ]; then
+if [ ${#} -ne 2 ]; then
+  echo "Error: Wrong number of arguments"
+  echo "Usage: ${0} <omnet-path> <omnet-project-root-dir>"
   exit 1
 fi
 
-# a single makefile with all the protocols. it overwrites the previous makefile. Build a shared library. Include path has a reference to omnet
-cd "${PROTOCOLS}" && ${OMNET_MAKEMAKE} -f --deep -s -I${INCLUDE} -I../base -L../base -lprotocol_base -O ${OUT_PATH}
-if [ $? -ne "0" ]; then
+if [ ! -d ${1} ]; then
+  echo "Invalid argument"
+  echo "${1} isn't a directory"
   exit 1
 fi
 
-# build library with all protocols
-cd "${PROTOCOLS}" && make -j ${CORES}
-if [ $? -ne "0" ]; then
+if [ ! -d ${2} ]; then
+  echo "Invalid argument"
+  echo "${2} isnt a directory"
   exit 1
 fi
+
+OMNET_PATH=${1}
+PROJECT_SRC=${2}
+INET="${OMNET_PATH}/samples/inet/src"
+if [ ! -f "${OMNET_PATH}/configure.user" -o ! -f "${OMNET_PATH}/include/omnetpp.h" ]; then
+  echo "Error: ${OMNET_PATH} directory does not look like an OMNeT++ root directory"
+  exit 1
+fi
+
+here=`pwd`; cd ../../; root=`pwd`; cd ${here}
+# opp_makemake -f --deep -s -o emergent_overlays -O ${root}/out -I${INET} -L${INET} && \
+cd ${PROJECT_SRC}/src && \
+  opp_makemake -f --deep -s -o emergent_overlays -I${INET} -L${INET} && \
+  make -j 4
+cd ${here}
