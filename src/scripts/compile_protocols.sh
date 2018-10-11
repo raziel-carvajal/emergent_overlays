39dda0226ec0f9b26bc485c4b23cfea66424c326
Raziel Carvajal-Gomez <raziel.carvajal@gmail.com>
2018-10-11 17:01:01 +0200

    WIP: fixing structure of files to compile algorithms

--- src/scripts/compile_protocols.sh
+++ src/scripts/compile_protocols.sh
@@ -19,17 +19,17 @@ if [ ! -d ${2} ]; then
 fi
 
 OMNET_PATH=${1}
-PROJECT_SRC=${2}/src
+PROJECT=${2}
 INET="${OMNET_PATH}/samples/inet/src"
 if [ ! -f "${OMNET_PATH}/configure.user" -o ! -f "${OMNET_PATH}/include/omnetpp.h" ]; then
   echo "Error: ${OMNET_PATH} directory does not look like an OMNeT++ root directory"
   exit 1
 fi
 
-here=`pwd`; cd ../../; root=`pwd`; cd ${here}
-cd ${PROJECT_SRC} && \
-  opp_makemake -I${INET} --projectdir . -f --deep --no-deep-includes -s -o emergent_overlays && \
-  make -j 4
+cd ${PROJECT_SRC}
+opp_makemake -I${INET} -I${PROJECT_SRC}/src -P ${PROJECT_SRC} --no-deep-includes -f --deep -s -o emergent_overlays
+make -j 4
+
 if [[ ${?} != 0 ]]; then
   echo "Error: during the compilation of emergent_overlays project"
   exit 1
