2d36ded9d087ee8c6dbda1778acf3b8dd7a1b12c
Raziel Carvajal-Gomez <raziel.carvajal@gmail.com>
2019-03-29 15:08:07 +0100

    fetch results from a any location in a remote server

--- src/scripts/fetch-results-from-remote-loc.sh
+++ src/scripts/fetch-results-from-remote-loc.sh
@@ -19,25 +19,26 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-if [ ${#} != 2 ] ; then
-  echo -e "Usage: ${0} <remote_server> <destination>\nEND of ${0}"
+if [ ${#} != 3 ] ; then
+  echo -e "Usage: ${0} <remote_server> <remote_location> <local_destination>\nEND of ${0}"
 	exit 1
 fi
-if [ ! -d ${2} ] ; then
-  echo -e "Error: ${2} isn't a vaild directory.\nEND of ${0}"
+if [ ! -d ${3} ] ; then
+  echo -e "Error: ${3} isn't a vaild directory.\nEND of ${0}"
 	exit 1
 fi
-remoteLoc=${1}
-destDir=${2}
-ssh ${remoteLoc} "[ -d emergent_overlays ]"
+server=${1}
+remoteDir=${2}
+destDir=${3}
+ssh ${server} "[ -d ${remoteDir}/src/emergent_overlays ]"
 if [ ${?} != 0 ] ; then
-  echo "Remote location (${remoteLoc}) do not contain framework: emergent_overlays"
-	echo -e "Any dataset will be fetch.\nEND of ${0}"
+  echo "Remote location (${remoteDir}) do not contain framework: emergent_overlays"
+	echo -e "Any dataset will be fetched.\nEND of ${0}"
 	exit 1
 fi
-ssh ${remoteLoc} "tar czf emergent_overlays/results.tgz emergent_overlays/results/"
-ssh ${remoteLoc} "tar czf emergent_overlays/experiments/networks/built_topologies.tgz \
-	emergent_overlays/experiments/networks/built_topologies"
-scp ${remoteLoc}:~/emergent_overlays/results.tgz ${destDir}
-scp ${remoteLoc}:~/emergent_overlays/experiments/networks/built_topologies.tgz ${destDir}
+ssh ${server} "tar czf ${remoteDir}/results.tgz ${remoteDir}/results/"
+ssh ${server} "tar czf ${remoteDir}/experiments/networks/built_topologies.tgz \
+	${remoteDir}/experiments/networks/built_topologies"
+scp ${server}:${remoteDir}/results.tgz ${destDir}
+scp ${server}:${remoteDir}/experiments/networks/built_topologies.tgz ${destDir}
 echo -e "Find dataset in ${destDir}\nEND of ${0}"
