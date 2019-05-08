b3a242f1654dc23172dcef4573f9e385b7f5cdbc
Raziel Carvajal-Gomez <raziel.carvajal@gmail.com>
2019-05-08 15:05:13 +0200

    deletion of deprecated source code

--- src/scripts/checking-depencencies.R
+++
@@ -1,7 +0,0 @@
-list.of.packages <- c("omnetpp", "argparse", "ggplot2", "reshape2", "grid", "gridExtra")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if (length(new.packages)) {
-	print (noquote("fail"))
-} else {
- 	print (noquote("Ok"))
-}
