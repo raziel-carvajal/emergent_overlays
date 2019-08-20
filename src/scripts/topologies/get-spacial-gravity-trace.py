838cf04dd2c8ccb32a8e9eee939ba106bb214e67
Raziel Carvajal-Gomez <raziel.carvajal@gmail.com>
2019-08-20 10:44:39 +0200

    a fraction of nodes keep moving randomly and the rest walk towards every PoI

--- src/scripts/topologies/get-spacial-gravity-trace.py
+++ src/scripts/topologies/get-spacial-gravity-trace.py
@@ -9,19 +9,21 @@ from pymobility.models.mobility import random_waypoint
 #
 G = nx.Graph()
 AGGREGATION = .5
+PARTITIONS = 4
 
 
 def getArgs():
   p = argparse.ArgumentParser(
-      description='Adhoc network with two Points of Interest (PoI) and N nodes. \
-      Initially, nodes move in a random way within the communication area. \
-      After a while, N/2 nodes walk towards one PoI and the remaining nodes \
-      follow the second PoI.')
+      description='Creates a mobility trace of an adhoc network with three \
+      Points of Interest (PoI) and N nodes. Initially, nodes move in a random \
+      way within the communication area. After a while, a quarter of the nodes \
+      remain moving randomly and the rest of nodes walk towards every PoI and \
+      the remaining nodes follow the second PoI.')
   p.add_argument(
-      '--cma-length', dest='area_l', type=int, default=100,
+      '--cma-length', dest='area_l', type=int, default=90,
       help='communication area length')
   p.add_argument(
-      '--cma-width', dest='area_w', type=int, default=100,
+      '--cma-width', dest='area_w', type=int, default=45,
       help='communication area width')
   p.add_argument(
       '--nodes', dest='nodes', type=int, default=100,
@@ -54,21 +56,47 @@ class CommunicationArea(object):
     self.length = length
     self.width = width
     self.nodes = nodes
+    self.subAreaLen = length / 2.0
+    self.subAreaWid = width / 2.0
+    self.subAreaOrigins = [(0, 0), (self.subAreaLen, 0),
+                           (0, self.subAreaWid)]
+    self.partitionsSize = [nodes / PARTITIONS] * PARTITIONS
     self.rand_model = self.getRandomWaypointInstance()
 
-  def setModelsOfAttraction(self, nodesAt1stPoI, posAt1stPoI, nodesAt2ndPoI, posAt2ndPoI):
-    self.first_model = reference_point_group(
-        nodesAt1stPoI, (self.length / 2, self.width), aggregation=AGGREGATION, initial_position=posAt1stPoI)
-    self.second_model = reference_point_group(
-        nodesAt2ndPoI, (self.length / 2, self.width), aggregation=AGGREGATION, initial_position=posAt2ndPoI)
+  def setModelsAtSubAreas(self, partitions):
+    self.groupModels = [
+        random_waypoint(
+            len(partitions[0]), (self.length, self.width), initial_position=partitions[0]
+        )
+    ]
+    self.groupModels.extend([
+        reference_point_group(
+            len(partitions[p]), (self.subAreaLen, self.subAreaWid),
+            aggregation=AGGREGATION, initial_position=partitions[p]
+        ) for p in range(1, len(partitions) - 1)
+    ])
+    self.groupModels.extend([
+        reference_point_group(
+            len(partitions[3]), (self.length, self.subAreaWid),
+            aggregation=AGGREGATION, initial_position=partitions[3]
+        )
+    ])
 
-  def getStepTowardsPoI(self, is1stGroup=True):
-    return [(p[0], p[1]) for p in next(self.first_model)] if is1stGroup else \
-        [(p[0] + (self.length / 2.), p[1]) for p in next(self.second_model)]
+  def makeStepAtSubareas(self, is1stGroup=True):
+    # 25% of nodes move in a random way
+    temp = [(p[0], p[1]) for p in next(self.groupModels[0])]
+    # the rest of the nodes move within their PoI
+    m = 1
+    for origin in self.subAreaOrigins:
+      temp.extend([
+          (p[0] + origin[0], p[1] + origin[1]) for p in next(self.groupModels[m])
+      ])
+      m = m + 1
+    return temp
 
   def updateNodePositions(self, atWholeNet=True):
-    self.positions = next(self.rand_model) if atWholeNet else \
-        self.getStepTowardsPoI() + self.getStepTowardsPoI(is1stGroup=False)
+    self.positions = [(p[0], p[1]) for p in next(self.rand_model)] if atWholeNet else \
+        self.makeStepAtSubareas()
 
   def getRandomWaypointInstance(self, initial_positions=[]):
     return random_waypoint(self.nodes, (self.length, self.width), initial_position=initial_positions)
@@ -93,12 +121,14 @@ if __name__ == '__main__':
       history[j].append((network.positions[j][0], network.positions[j][1]))
     i = i + 1
   # get instances of a mobility model where nodes move towards PoI
-  posAt1stGroup, posAt2ndGroup = [], []
-  for p in network.positions:
-    l = posAt1stGroup if p[0] <= (network.length / 2.) else posAt2ndGroup
-    l.append((p[0], p[1]))
-    network.setModelsOfAttraction(
-        len(posAt1stGroup), posAt1stGroup, len(posAt2ndGroup), posAt2ndGroup)
+  j = 0
+  partitions = []
+  for p in range(0, PARTITIONS):
+    partitions.append([])
+    for _ in range(0, network.partitionsSize[p]):
+      partitions[p].append(network.positions[j])
+      j = j + 1
+  network.setModelsAtSubAreas(partitions)
   # plot snapshots of such network
   while i < args.rand_steps + args.poi_steps:
     network.updateNodePositions(atWholeNet=False)
