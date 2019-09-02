b48c4c5c2dc36ff6666ff9cd263a999196f7f552
Raziel Carvajal-Gomez <raziel.carvajal@gmail.com>
2019-09-02 16:06:43 +0200

    add scenario ID in the creation of trace/configuration file

--- services/trace_generator/run_trace_generator.sh
+++ services/trace_generator/run_trace_generator.sh
@@ -49,7 +49,7 @@ if [[ "${USE_PREVIOUS_TRACE}" != "yes" ]]; then
 			# ./make-topology.sh
 			;;
 	esac
-	./create_omnet_cfg_files.sh
+	./create_omnet_cfg_files.sh ${SCENARIO_ID}
 else
 	echo "Using previous trace..."
 fi
--- src/scripts/topologies/create_omnet_cfg_files.sh
+++ src/scripts/topologies/create_omnet_cfg_files.sh
@@ -16,6 +16,7 @@
 #       CREATED: 08/13/2019 14:08
 #      REVISION:  ---
 #===============================================================================
+[ ${#} != 1 ] && echo -e "Usage: ${0} [Scenario_ID] \nEnd of ${0}." && exit 1
 # [BEING] NOTE uncomment this block to perform a local test
 # rm -f *.pdf *.gif *.bm *.ned \
 # 	network_metadata.json bipartite-scenario-1st-position source_nodes.xml
@@ -41,7 +42,7 @@ mv ${expeId}.* source_nodes.xml ../../../experiments/networks/built_topologies
 cat '../../../experiments/configs/in_common/common.ini' > iniFile
 ###
 sed -i -e "s/SIMULATION_TIME/${SIMULATION_TIME}s/" iniFile
-sed -i -e "s/CONFIGURATION_NAME/${expeId}poiscenario/" iniFile
+sed -i -e "s/CONFIGURATION_NAME/${expeId}${1}/" iniFile
 sed -i -e "s/TOPOLOGY_NAME/${expeId}/" iniFile
 sed -i -e "s/NODES_TRANSMISSION_RANGE/${NODES_TRANSMISSION_RANGE}m/" iniFile
 sed -i -e "s/BROADCAST_MSG_INTERVAL/${BROADCAST_MSGS_INTERVAL}s/" iniFile
@@ -51,13 +52,26 @@ sed -i -e "s/ADAPTATION_POLICY/0/" iniFile
 ### number of nodes in the network
 echo "**.udpApp[0].nodesNo = ${NODES}" >> iniFile
 ### assign the broadcast protocol running on each node
-algorithms="../../../experiments/configs/in_common/algo_class_mapping"
-algoId=`grep ${ALGO_AT_1ST_REGION} ${algorithms} | awk -F "=" '{print $2}'`
-let N=NODES/2
-echo "*.host{1..${N}}.udpApp[0].runningProtocolId = ${algoId}" >> iniFile
-algoId=`grep ${ALGO_AT_2ND_REGION} ${algorithms} | awk -F "=" '{print $2}'`
-let N=N+1
-echo "*.host{${N}..${NODES}}.udpApp[0].runningProtocolId = ${algoId}" >> iniFile
+case "${1}" in
+	"with1Poi" )
+		echo "Nodes use pure-flooding as protocol to bootstrap"
+		;;
+	"with2Poi" )
+    # TODO
+		;;
+	"bipartite" )
+		algorithms="../../../experiments/configs/in_common/algo_class_mapping"
+		algoId=`grep ${ALGO_AT_1ST_REGION} ${algorithms} | awk -F "=" '{print $2}'`
+		let N=NODES/2
+		echo "*.host{1..${N}}.udpApp[0].runningProtocolId = ${algoId}" >> iniFile
+		algoId=`grep ${ALGO_AT_2ND_REGION} ${algorithms} | awk -F "=" '{print $2}'`
+		let N=N+1
+		echo "*.host{${N}..${NODES}}.udpApp[0].runningProtocolId = ${algoId}" >> iniFile
+		;;
+	* )
+		echo "Nodes use pure-flooding as protocol to bootstrap"
+		;;
+esac
 ### assign one mobility trace per node
 mobModel="# tunning mobility model\n*.host*.mobilityType = \"BonnMotionMobility\"\n"
 traceFilePath="../../experiments/networks/built_topologies/"
@@ -68,7 +82,7 @@ done
 mobModel=${mobModel}"**.udpApp[0].motionInterval=1s\n"
 echo -e ${mobModel} >> iniFile
 ### change containing directory of INI file
-mv iniFile "${expeId}poiscenario.ini"
-mv "${expeId}poiscenario.ini" ../../../experiments/configs/built_configs
+mv iniFile "${expeId}${1}.ini"
+mv "${expeId}${1}.ini" ../../../experiments/configs/built_configs
 ### job for worker
-echo "${expeId}poiscenario.ini" > "../../../experiments/configs/built_configs/cfgs_for_workers"
+echo "${expeId}${1}.ini" > "../../../experiments/configs/built_configs/cfgs_for_workers"
